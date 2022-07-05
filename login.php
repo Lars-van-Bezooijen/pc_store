a38acd71cedf5a5e76c3489813ff1b6f402c3b0f
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-07-05 19:10:08 +0200

    added all pages

---
+++ login.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+	<?php require_once 'head.php'; ?>
+	<title>PC Today - Login</title>
+</head>
+
+<body>
+    <?php 
+    session_start(); 
+    require_once 'backend/conn.php'; 
+    require_once 'header.php'; 	
+    ?>
+
+
+
+
+
+
+
+
+
+
+
+    <?php require_once 'scripts.php'; ?>
+</body>
+
+</html>
---
+++ shop.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+	<?php require_once 'head.php'; ?>
+	<title>PC Today - Shop</title>
+</head>
+
+<body>
+	<?php 
+	require_once 'backend/conn.php'; 
+	require_once 'header.php'; 	
+	?>
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	<?php require_once 'scripts.php'; ?>
+</body>
+
+</html>
