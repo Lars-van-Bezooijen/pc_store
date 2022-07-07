cbd3a686e24da1c2c17a135c8957cc0ae0d90e50
Lars-van-Bezooijen <latsvb10@gmail.com>
2022-07-07 19:13:19 +0200

    added product page with category filter

--- shop.php
+++ shop.php
@@ -12,24 +12,74 @@
 	require_once 'backend/conn.php'; 
 	require_once 'header.php'; 	
 	?>
-	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	<div class="container flex">	
+		<div class="filter">
+			<form action="" method="GET">
+				<div class="filter-group">
+					<label for="category">Category</label>
+					<select name="category" id="category" value="">
+						<option hidden disabled selected value> -- choose category -- </option>
+						<?php
+						$query = "SELECT * FROM categories";
+						$statement = $conn->prepare($query);
+						$statement->execute([]);
+						$categories = $statement->fetchAll(PDO::FETCH_ASSOC);
+						foreach($categories as $category)
+						{
+							?>
+							<option value="<?php echo $category['id']; ?>"><?php echo $category['type']; ?></option>
+							<?php
+						}
+						?>
+					</select>
+				</div>
+				<input type="submit" value="Verzend">
+			</form>
+		</div>	
+		
+		<div class="grid">
+			<?php
+			if(isset($_GET['category']))
+			{
+				$category = $_GET['category'];
+			}
+			else
+			{
+				$category = "categories.id";
+			}
+			$query = "SELECT products.id, products.name, products.image, products.stock, products.price, products.price_off, categories.type
+			FROM products INNER JOIN categories ON products.category = categories.id WHERE categories.id = $category";
+			$statement = $conn->prepare($query);
+			$statement->execute([]);
+			$products = $statement->fetchAll(PDO::FETCH_ASSOC);
+			foreach($products as $product)
+			{
+				?>
+				<div class="product">
+					<a href="product.php?id=<?php echo $product['id']; ?>">
+						<img src="img/products/<?php echo $product['image'];?>" alt="">
+					</a>
+					<div class="info">
+						<div class="side">
+							<a class="underline" href="product.php?id=<?php echo $product['id']; ?>">
+								<p class="title"><?php echo $product['name']; ?></p>
+							</a>
+						</div>
+						<div class="side">
+							<a href="product.php?id=<?php echo $product['id']; ?>">
+								<p class="price"><?php echo $product['price']; ?></p>
+							</a>
+							<p>CART BTN</p>
+							<!-- <i class="fa-solid fa-cart-shopping"></i> -->
+						</div>
+					</div>
+				</div>
+				<?php
+			}
+			?>
+		</div>
+	</div>
 	<?php require_once 'scripts.php'; ?>
 </body>
 
